138cac1ba5ab93a8e14409bb1eed3cd8b410119d
Evren Keskin <evrenkeskin0998@gmail.com>
2022-11-06 15:16:04 +0300

    addNewBookmark()

--- popup.js
+++ popup.js
@@ -1,6 +1,20 @@
 import { getCurrentTab } from "./utils.js";
 // adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+const addNewBookmark = (bookmarksElement, bookmark) => {
+  const bookmarkTitleElement = document.createElement("div");
+  const newBookmarkElement = document.createElement("div");
+  const controlsElement = document.createElement("div");
+  bookmarkTitleElement.textContent = bookmark.desc;
+  bookmarkTitleElement.className = "bookmark-title";
+  controlsElement.className = "bookmark-controls";
+  newBookmarkElement.id = "bookmark-" + bookmark.time;
+  newBookmarkElement.className = "bookmark";
+  newBookmarkElement.setAttribute("timestamp", bookmark.time);
+  setBookmarkAttributes("play", onPlay, controlsElement);
+  newBookmarkElement.appendChild(bookmarkTitleElement);
+  newBookmarkElement.appendChild(controlsElement);
+  bookmarksElement.appendChild(newBookmarkElement);
+};
 
 const viewBookmarks = (currentBookmarks = []) => {
   const bookmarkElement = document.getElementsById("bookmarks");
@@ -16,11 +30,25 @@ const viewBookmarks = (currentBookmarks = []) => {
   }
 };
 
-const onPlay = (e) => {};
+const onPlay = async (e) => {
+  const bookmarkTime = e.target.parentNode.parentNode.getAttribute("timestamp");
+  const activeTab = await getActiveTabURL();
+  chrome.tabs.sendMessage(activeTab.id, {
+    type: "PLAY",
+    value: bookmarkTime,
+  });
+};
 
 const onDelete = (e) => {};
 
-const setBookmarkAttributes = () => {};
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+  const controlElement = document.createElement("img");
+
+  controlElement.src = "assets/" + src + ".png";
+  controlElement.title = src;
+  controlElement.addEventListener("click", eventListener);
+  controlParentElement.appendChild(controlElement);
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveTabURL();
